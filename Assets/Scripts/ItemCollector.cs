198233f048b1617fd5c128c208a7dc0a22b76bd1
irem <iremaydinn.7@gmail.com>
2022-11-08 17:20:58 +0300

    traps added

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 { 
     private int cherries = 0;
-    [SerializeField] private Text cherriesText;
+    [SerializeField] private TextMeshProUGUI cherriesText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
