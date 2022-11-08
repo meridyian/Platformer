6b3d1d8ee2b69f73b0c5fb4d82908cb7c69477da
irem <iremaydinn.7@gmail.com>
2022-11-08 10:40:54 +0300

    movement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //execute your rb at start of the game
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //jump during the game but dont when you pressed down
-        if (Input.GetKeyDown("space"))
+        // directional variable range(-1,1)
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        
+
+        //jump during the game but dont when you pressed down with input manager
+        if (Input.GetButtonDown("Jump"))
         {
             // to access, only jump
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
     }
 }
