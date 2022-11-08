fa9f104859c04ac28277d191a9e97a2e8707abee
irem <iremaydinn.7@gmail.com>
2022-11-08 10:25:54 +0300

    getkeydown

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //jump during the game
-        if (Input.GetKey("space"))
+        //jump during the game but dont when you pressed down
+        if (Input.GetKeyDown("space"))
         {
             // to access, only jump
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
         }
     }
 }
